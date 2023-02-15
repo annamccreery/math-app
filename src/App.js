1fc4aff918c1a3f1368245b697460a1a13911eb2
Anna McCreery <amccreery@madisoncollege.edu>
2023-02-15 15:13:14 -0600

    updating styles and limiting decimal places

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import './App.css';
 import { Math } from './Math'
 
 const App = () => {
-  const styles = {
-    container: {
-      padding: 20,
-      border: "2px solid pink"
-    }
-  }
-
   return (
-    <div style={styles.container}>
+    <div>
       <Math numberOne={10} numberTwo={20} />
-      <Math numberOne={50} numberTwo={100} />
+      <Math numberOne={1} numberTwo={18} />
     </div>
   );
 };
--- src/Math.js
+++ src/Math.js
@@ -4,16 +4,16 @@ export const Math = ({numberOne, numberTwo}) => {
     return (
         <div>
             <h2>
-                {numberOne} + {numberTwo} = {numberOne + numberTwo}
+                {numberOne} + {numberTwo} = {(numberOne + numberTwo).toFixed(3)}
             </h2>
             <h2>
-                {numberOne} - {numberTwo} = {numberOne - numberTwo}
+                {numberOne} - {numberTwo} = {(numberOne - numberTwo).toFixed(3)}
             </h2>
             <h2>
-                {numberOne} * {numberTwo} = {numberOne * numberTwo}
+                {numberOne} * {numberTwo} = {(numberOne * numberTwo).toFixed(3)}
             </h2>
             <h2>
-                {numberOne} / {numberTwo} = {numberOne / numberTwo}
+                {numberOne} / {numberTwo} = {(numberOne / numberTwo).toFixed(3)}
             </h2>
         </div>
     );
