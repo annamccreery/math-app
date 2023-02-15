ad0d620be582e870da3f02e7cf607dbeb56ce7a1
Anna McCreery <amccreery@madisoncollege.edu>
2023-02-15 12:30:48 -0600

    created addition.js and tested

---
+++ src/Addition.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Addition = () => {
+    return 10 + 20
+}
+
+export default Addition;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
+import Addition from './Addition'
 
 const App = () => {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <>
+      <Addition />
+    </>
   );
 }
 
